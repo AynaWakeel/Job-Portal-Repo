b78c00df15baa0e6508660627417532044813391
Ayna Wakeel <aynawakeel4@gmail.com>
2025-09-24 19:02:05 +0500

    applicant applied jobs

--- src/components/applied-jobs/index.jsx
+++ src/components/applied-jobs/index.jsx
@@ -1,56 +1,70 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { MainSec } from './style'
 import StatusClose from '../../assets/icons/XCircleRed.svg'
 import { AppliedJobsCards } from '../../helper/dummyData'
+import Check from '../../assets/icons/Check.svg'
 import { useNavigate } from 'react-router'
+import { Applicant_Endpoints } from '../../lib/api/applicant_endpoints'
 
-const AppliedJobs = ({ limit }) => {
+const AppliedJobs = () => {
     const [isActive, setIsActive] = useState("Active")
-    const [Status, setStatus] = useState("Rejected")
-    // const [jobData, setJobData] = useState({})
+    // const [Status, setStatus] = useState("Rejected")
+    const [jobData, setJobData] = useState([])
+
+     useEffect(() => {
+        const fetchData = async () => {
+            const res = await Applicant_Endpoints.get_jobs()
+            if (res?.data?.jobs) {
+                setJobData(res.data.jobs)
+            }
+        }
+        fetchData()
+    }, [])
 
     const navigate = useNavigate()
     const ViewDetail = () => {
         navigate('/applicant/company')
     }
-    const ShowCards = limit ? AppliedJobsCards.slice(0, limit) : AppliedJobsCards;
     return (
         <div>
             <MainSec>
                 <div className='CardDiv'>
                     <div className='Grid'>
-                        {ShowCards.map((items) => {
+                        {jobData.map((items) => {
                             return (
                                 <div className='Card'>
                                     <div className='Inner-flex'>
-                                        <div className='IconBox' style={{ backgroundColor: `${items.color}` }}>
-                                            <img src={items.logo} />
+                                        <div className='IconBox' 
+                                        // style={{ backgroundColor: `${items.color}` }}
+                                        >
+                                            <img src={items.recruiter.profilepic} />
                                         </div>
                                         <div className='Gap'>
                                             <div className='Inner-flex'>
                                                 <h3 className='Heading'>{items.title}</h3>
-                                                <span className='Badge'>{items.badge}</span>
+                                                <span className='Badge'>{items.jobType}</span>
                                             </div>
                                             <div className='Inner-flex'>
-                                                {items.detail.slice(0, 2).map((d) => (
                                                     <h4 className='FlexIcon'>
-                                                        <span><img src={d.icon} /></span>
-                                                        <span className='SubHeading'>{d.text}</span>
+                                                        <span><img src='' /></span>
+                                                        <span className='SubHeading'>{items.location}</span>
                                                     </h4>
-
-                                                ))}
+                                                     <h4 className='FlexIcon'>
+                                                        <span><img src='' /></span>
+                                                        <span className='SubHeading'>${items.salaryMin} - ${items.salaryMax}</span>
+                                                    </h4>
                                             </div>
                                         </div>
                                     </div>
                                     <div>
-                                        <span className='SubHeading'>{items.date}</span>
+                                        <span className='SubHeading'>{items.jobExpirationDate}</span>
                                     </div>
 
                                     <div className='status-flex'>
                                         {isActive === "Active" ?
 
                                             (<div className='Activediv'>
-                                                <span><img src={items.icon} alt='icon' /></span>
+                                                <span><img src={Check} alt='icon' /></span>
                                                 <span className='Active'>Active</span>
                                             </div>)
 
@@ -63,7 +77,7 @@ const AppliedJobs = ({ limit }) => {
 
                                         }
 
-                                         {Status === "Selected" &&
+                                         {items.status === "Selected" &&
 
                                             (<div className='Activediv'>
                                                 <span><img src={items.icon} alt='icon' /></span>
@@ -72,7 +86,7 @@ const AppliedJobs = ({ limit }) => {
 
                                         }
 
-                                         {Status === "Rejected" &&
+                                         {items.status === "Rejected" &&
 
                                             (<div className='Activediv'>
                                                 <span><img src={StatusClose} alt='icon' /></span>
@@ -81,7 +95,7 @@ const AppliedJobs = ({ limit }) => {
 
                                         }
 
-                                         {Status === "Applied" &&
+                                         {items.status === "Applied" &&
 
                                             (<div className='Activediv'>
                                                 <span><img src={items.icon} alt='icon' /></span>
@@ -93,7 +107,7 @@ const AppliedJobs = ({ limit }) => {
 
                                     <div className='Right-side'>
                                         <button className='CardBtn' onClick={ViewDetail}>
-                                            <span>{items.btn}</span>
+                                            <span>View Details</span>
                                         </button>
                                     </div>
                                 </div>
